09a45f5a2de352aaa106996831e0ca2a0f971548
serotonine <ks.serotonine@gmail.com>
2025-11-26 21:30:46 +0100

    Sprint1: exercise-3.

--- Sprint-1/destructuring/exercise-3/exercise.js
+++ Sprint-1/destructuring/exercise-3/exercise.js
@@ -6,3 +6,13 @@ let order = [
   { itemName: "Hot Coffee", quantity: 2, unitPricePence: 100 },
   { itemName: "Hash Brown", quantity: 4, unitPricePence: 40 },
 ];
+const setPrice = (pricePence) => (pricePence / 100).toFixed(2)
+let total = 0;
+console.log(`${("QTY").padEnd(7)}${"ITEM".padEnd( 20)}TOTAL`);
+for (let item of order){
+  const {itemName, quantity, unitPricePence} = item;
+  const sousTotal = (quantity * unitPricePence);
+  total += sousTotal;
+  console.log(`${(quantity+"").padEnd(7)}${itemName.padEnd(20)}${setPrice(sousTotal)}`);
+}
+console.log(`\n${("TOTAL").padEnd(26)}${setPrice(total)}`);
