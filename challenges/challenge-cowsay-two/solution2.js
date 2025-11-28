5a131f56e57537a23c0b41c10f4d149313f94d28
serotonine <ks.serotonine@gmail.com>
2025-11-28 15:03:58 +0100

    challenges: cowsay / solution 2.

--- challenges/challenge-cowsay-two/solution2.js
+++ challenges/challenge-cowsay-two/solution2.js
@@ -1,18 +1,48 @@
+import readlineSync from 'readline-sync';
 // =================
 // Stripped down cowsayer CLI, 
-// no libraries or arguments
-// https://nodejs.dev/learn/accept-input-from-the-command-line-in-nodejs
+// no libraries
+// https://nodejs.dev/learn/nodejs-accept-arguments-from-the-command-line
 // =================
 
-// 1. Make  a command line interface.
+function drawBubble(str, lg){
+  if(!str){return}
+  let bubble = "";
+  bubble += ` ${"–".repeat(lg + 2)} \n`;
+  bubble += `< ${str} >\n`;
+  bubble += ` ${"-".repeat(lg + 2)}`;
+  console.log(bubble);
 
-// 2. Make supplies for our speech bubble
-
-// 3. Make a cow that takes a string
-
-const cow = (saying) => {
-    // how did you make the cow before?
 }
 
-// 4. Use readline to get a string from the terminal 
-// (with a prompt so it's clearer what we want)
+function drawCow(talk, lg=0){
+  const prefix = (" ").repeat(lg + 2 - Math.round(lg / 2));
+  const stucks = [
+    `${talk?"\\  ^——^":"   ^——^"}\n`,
+    `${talk?" \\ (oo)\________":"   (oo)\________"}\n`,
+    `   (__)\\       )\\/\\\n`,
+    `       ||----w |\n`,
+    `       ||     ||`
+  ].map((row)=> prefix + row);
+console.log(stucks.join(""));
+
+}
+
+function cowsay() {
+    drawCow(false);
+  // Get console argument.
+  const talk = readlineSync.question('Wazz up Miss Cow? ');
+  // If no argument.
+  if(!talk){
+    console.log("Miss Cow is not very talkative...");
+    return;
+  }
+  const lg = talk.length;
+  
+  setTimeout(()=>{
+    drawBubble(talk,lg);
+    drawCow(true, lg);
+  }, 1000);
+}
+
+cowsay();
