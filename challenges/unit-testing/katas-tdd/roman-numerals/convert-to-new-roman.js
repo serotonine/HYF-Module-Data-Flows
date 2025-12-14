31cc470ad618b234a27787c794e195ed9feab2a9
serotonine <ks.serotonine@gmail.com>
2025-12-14 15:55:56 +0100

    Katas: convert to new roman + test.

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.js
@@ -1,39 +1,27 @@
-function convertToNewRoman(n) {}
+function convertToNewRoman(n) {
+  const map = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let str = "";
+  for (var i in map) {
+    while (n >= map[i]) {
+      str += i;
+      n -= map[i];
+    }
+  }
+  return str;
+}
 
 module.exports = convertToNewRoman;
-
-/* Roman Numerals Helper */
-/* I	V	 X	 L	 C	 D	  M
- * 1	5	10	50	100	500	1000 */
-
-const map = { M:1000, CM:900, D:500, CD:400, C:100, XC:90, L:50, XL:40, X:10, IX:9, V:5, IV:4, I:1};
-
-class RomanNumerals {
-    static toRoman(num) {
-        let str = '';
-        // Loop on index.
-        for (var i in map) {
-          
-            while (num >= map[i]) {
-              console.log("map",map[i]);
-                str += i;
-              num -= map[i];
-              console.log("num",num);
-            }
-        }
-        return str;
-    }
-    
-    static fromRoman(str) {
-      console.log("match", str.match(/CM|CD|XC|XL|IX|IV|\w/g));
-         return str.match(/CM|CD|XC|XL|IX|IV|\w/g).reduce((acc, el) => acc + map[el], 0);
-    }
-}
-console.log(RomanNumerals.fromRoman("MMMCMXCIX"));
-//console.log(RomanNumerals.toRoman(3999));
-/* 
-486 => CDLXXXVI
-3999 => MMMCMXCIX
-666 => DCLXVI
-465 => CDLXV
-  */
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-new-roman.test.js
@@ -1,7 +1,21 @@
 let convertToNewRoman = require("./convert-to-new-roman");
 
-test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  // Act
-  // Assert
+test("returns I if passed 1 as an argument", () => {
+  expect(convertToNewRoman(1)).toEqual("I");
+});
+
+test("returns IV if passed 4 as an argument", () => {
+  expect(convertToNewRoman(4)).toEqual("IV");
+});
+
+test("returns XLIV if passed 44 as an argument", () => {
+  expect(convertToNewRoman(44)).toEqual("XLIV");
+});
+
+test("returns MDLXVII if passed 1567 as an argument", () => {
+  expect(convertToNewRoman(1567)).toEqual("MDLXVII");
+});
+
+test("returns CMXCIX if passed 999 as an argument", () => {
+  expect(convertToNewRoman(999)).toEqual("CMXCIX");
 });
