fa75059f68e98cdea3ecba077bd2fe61c0dceaa7
serotonine <ks.serotonine@gmail.com>
2025-12-14 15:44:39 +0100

    Katas: convert to old roman + test.

--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.js
@@ -1,3 +1,31 @@
-function convertToOldRoman(n) {}
+/*
+ * We are going to begin writing a function `convertToOldRoman`.
+ * When passed an integer between 1 and 3000, this function will
+ * return a string containing the proper old Roman Numeral.
+ * In other words, `convertToOldRoman(4)` should return the string `'IIII'`.
+ */
+
+function convertToOldRoman(n) {
+  const map = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    C: 100,
+    XC: 90,
+    L: 50,
+    X: 10,
+    IX: 9,
+    V: 5,
+    I: 1,
+  };
+  let str = "";
+  for (var i in map) {
+    while (n >= map[i]) {
+      str += i;
+      n -= map[i];
+    }
+  }
+  return str;
+}
 
 module.exports = convertToOldRoman;
--- challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
+++ challenges/unit-testing/katas-tdd/roman-numerals/convert-to-old-roman.test.js
@@ -1,7 +1,17 @@
 let convertToOldRoman = require("./convert-to-old-roman");
 
-test("returns I if passed 1 as an argument", function () {
-  // Arrange
-  // Act
-  // Assert
+test("returns I if passed 1 as an argument", () => {
+  expect(convertToOldRoman(1)).toEqual("I");
+});
+
+test("returns IIII if passed 4 as an argument", () => {
+  expect(convertToOldRoman(4)).toEqual("IIII");
+});
+
+test("returns XXXXIIII if passed 44 as an argument", () => {
+  expect(convertToOldRoman(44)).toEqual("XXXXIIII");
+});
+
+test("returns MDLXVII if passed 1567 as an argument", () => {
+  expect(convertToOldRoman(1567)).toEqual("MDLXVII");
 });
