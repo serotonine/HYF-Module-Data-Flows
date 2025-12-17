b062874543e82e7453576f28466378cd66638a4a
serotonine <ks.serotonine@gmail.com>
2025-12-17 11:52:53 +0100

    Passing-test: car-sales def.

--- challenges/unit-testing/passing-tests/car-sales/car-sales.js
+++ challenges/unit-testing/passing-tests/car-sales/car-sales.js
@@ -1,28 +1,16 @@
 function sales(carsSold) {
   const results = new Map();
-  for (let car of carsSold){
-    const {make:brand, price } = car;
-    if(results.has(brand)){
+  for (let car of carsSold) {
+    const { make: brand, price } = car;
+    if (results.has(brand)) {
       const acc = results.get(brand);
-      console.log("price already set", acc);
       results.set(brand, acc + price);
-
+    } else {
+      results.set(brand, price);
     }
-    else{results.set(brand, price)}
   }
-  console.log(results);
+  const sorted = Array.from(results).sort((a, b) => a[0].localeCompare(b[0]));
+  return Object.fromEntries(sorted);
 }
-
-let carsSold = [
-    { make: "Ford", model: "Fiesta", colour: "Red", price: 5999 },
-    { make: "Land Rover", model: "Defender", colour: "Muddy", price: 12000 },
-    { make: "Toyota", model: "Prius", colour: "Silver", price: 6500 },
-    { make: "Honda", model: "Civic", colour: "Yellow", price: 8000 },
-    { make: "Ford", model: "Fiesta", colour: "Red", price: 15000 },
-    { make: "Land Rover", model: "Discovery", colour: "Blue", price: 9000 },
-    { make: "Ford", model: "Fiesta", colour: "Green", price: 2000 },
-  ];
-
-  sales(carsSold);
 
 module.exports = sales;
