d143b20dd74d27c9fa66710e690cade7813871b2
serotonine <ks.serotonine@gmail.com>
2025-12-17 11:59:15 +0100

    Passing-test: factorial def.

--- challenges/unit-testing/passing-tests/factorial/factorial.js
+++ challenges/unit-testing/passing-tests/factorial/factorial.js
@@ -1,6 +1,6 @@
 // int is an integer
 // a factorial is the product of all non-negative integers
-// less than or equal to the iniital number.
+// less than or equal to the initial number.
 
 // for example the factorial of 5 is 120
 // 120 = 1 * 2 * 3 * 4 * 5
@@ -8,6 +8,13 @@
 // calculate and return the factorial of int
 // note: factorial of 0 is 1
 
-function factorial(int) {}
+function factorial(int) {
+  let result = 1;
+  while(int>0){
+    result *= int;
+    int--;
+  }
+  return result;
+}
 
 module.exports = factorial;
