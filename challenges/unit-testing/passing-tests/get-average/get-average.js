a670fa3159adb023462ca60ea0634e4d648e6407
serotonine <ks.serotonine@gmail.com>
2025-12-17 13:26:02 +0100

    Passing-test: get-average def.

--- challenges/unit-testing/passing-tests/get-average/get-average.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.js
@@ -2,6 +2,9 @@
 // return the average of all the numbers
 // be sure to exclude the strings
 
-function average(numbers) {}
+function average(numbers) {
+  const nb = numbers.filter((item) => typeof item !== "string");
+  return Math.round(nb.reduce((acc, current) => acc + current, 0) / nb.length);
+}
 
 module.exports = average;
--- challenges/unit-testing/passing-tests/get-average/get-average.test.js
+++ challenges/unit-testing/passing-tests/get-average/get-average.test.js
@@ -1,10 +1,13 @@
 let average = require("./get-average");
 
 test("Average", function () {
-  let numbers = [4, "-", 8, 11, "hello", "57", 0, 2];
-  let expected = 5;
-
-  let output = average(numbers);
-
-  expect(output).toEqual(expected);
+  const tests = [
+    [4, "-", 8, 11, "hello", "57", 0, 2],
+    [5678, "ccc", -24, 5, "678", "Sidonie & Aglaé"],
+    [-567, -8, -11, "57", "", 2],
+  ];
+  const expected = [5, 1886, -146];
+  for (let i = 0; i < tests.length; i++) {
+    expect(average(tests[i])).toEqual(expected[i]);
+  }
 });
